d60c768ef1a0507c5746cda49c8b642d9297ae52
Tony Fabery <tfabery@gmail.com>
2016-06-03 10:16:07 -0700

    add styling to hide destination divs and script.js for testing form submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 // Backend logic
+var name;
+var month;
 
 // Frontend logic
+$(function() {
+  $('form#vac-form').submit(function(event){
+    name = $('#name').val();
+    month = $('#month').val();
+    console log(name);
+    console log(month);
+    event.preventDefault();
+  });
+});
