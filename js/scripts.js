a0372f26474fa447e4ee6ceee4ad7946d342618e
Tony Fabery <tfabery@gmail.com>
2016-06-03 11:32:25 -0700

    fix radio selection values for name=weather and add variables for all inputs and tests in scripts.js and add if else statements to show destination div and hide form div

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 // Backend logic
 var name;
 var month;
+var landscape;
+var tv;
+var weather;
 
 var getMonth = function(date) {
   var mm = date.substr(5, 2);
@@ -13,8 +16,14 @@ $(function() {
     var startDate = $('#startDate').val();
     name = $('#name').val();
     month = getMonth(startDate);
+    landscape = $('input:radio[name=landscape]:checked').val();
+    tv = $('input:radio[name=tv]:checked').val();
+    weather = $('input:radio[name=weather]:checked').val();
     console.log(name);
     console.log(startDate);
     console.log(month);
+    console.log(landscape);
+    console.log(tv);
+    console.log(weather);
   });
 });
