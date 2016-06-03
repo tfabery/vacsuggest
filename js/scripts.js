50a33283a423ba82bdde6728ab201141bbb43cbe
Tony Fabery <tfabery@gmail.com>
2016-06-03 15:31:12 -0700

    add hidden jumbotron and make it appear when form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ var month;
 var landscape;
 var tv;
 var weather;
+var destination;
 
 var getMonth = function(date) {
   var mm = date.substr(5, 2);
@@ -12,22 +13,28 @@ var getMonth = function(date) {
 
 var destination = function() {
   if (weather === 'cold') {
-    $('#antarctica').show();
+    destination = 'Antarctica';
+    $('#antarctica').slideDown();
+    $('.jumbotron').fadeIn();
     $('.form').hide();
   }
   else if (landscape === 'desert') {
+    destination = 'Egypt';
     $('#egypt').show();
     $('.form').hide();
   }
   else if (tv === 'anime') {
+    destination = 'Japan';
     $('#japan').show();
     $('.form').hide();
   }
   else if ((tv === 'animals' && weather === 'warm' && ((month >= 3 && month <= 5) || (month >= 9 && month <= 11))) || (tv === 'animals' && weather === 'hot' && (month ===12 || month <= 2))) {
+    destination = 'New Zealand';
     $('#newzealand').show();
     $('.form').hide();
   }
   else {
+    destination = 'Bahamas';
     $('#bahamas').show();
     $('.form').hide();
   }
@@ -53,5 +60,7 @@ $(function() {
     // console.log(weather);
 
     destination();
+    $('.name').text(name);
+    $('.destination').text(destination);
   });
 });
